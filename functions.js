87e303530b25b6e5a9e201be99e9a44d8360845e
Ikechukwu Nwakanma <icnwakanma@gmail.com>
2018-11-07 11:37:01 +0100

    update calculation

--- functions.js
+++ functions.js
@@ -2,19 +2,56 @@
 $(document).ready(function() {
 
   $('input[type=range]').rangeslider({
-    polyfill: false
+    polyfill: false,
+    onSlide: function(position, value) {
+
+        var budget = value.toLocaleString();
+
+        $('.price').text(budget);
+        calculatorInput = value;
+        calculateBudget();
+      }
   });
+
+  var scopeCalculator = 'facebook';
+  var calculatorInput = 10000;
+  var calculatorFactor = 1;
+
+  function calculateBudget(){
+    if (scopeCalculator === 'facebook') {
+      var calcOutput = calculatorInput / calculatorFactor * 1.2;
+      output = calcOutput.toFixed(0);
+      $('.estimate-price').text(numberWithCommas(output) + ' Reach')
+    }
+    else if (scopeCalculator === 'twitter') {
+        var calcOutput = calculatorInput / calculatorFactor * 1.3;
+        output = calcOutput.toFixed(0);
+        $('.estimate-price').text(numberWithCommas(output) + ' Reach')
+      }
+    else if (scopeCalculator === 'instagram') {
+      var calcOutput = calculatorInput / calculatorFactor * 1.3;
+      output = calcOutput.toFixed(0);
+      $('.estimate-price').text(numberWithCommas(output) + ' Reach')
+    }
+    else if (scopeCalculator === 'youtube') {
+      var calcOutput = calculatorInput / calculatorFactor * 5;
+      output = calcOutput.toFixed(0);
+      $('.estimate-price').text(numberWithCommas(output) + ' Reach')
+    }
+  }
+
+  const numberWithCommas = (x) => {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
+
   var range = $('.input-range');
   var value = $('.price');
   var x = value.html(range.attr('value'));
-  range.on('input', function(){
-        value.html(this.value);
-    });
 
     $('.channel .btn').click(function(){
-
         $('.channel .btn').removeClass('active');
         $(this).addClass('active');
-
+        scopeCalculator = $(this).attr('scope');
+        calculateBudget();
       });
 });
